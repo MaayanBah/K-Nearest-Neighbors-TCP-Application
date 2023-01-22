bab7e61bd4a416eb46e828c35a47e0a270e4bbf1
maayan <maayanbah@gmail.com>
2023-01-23 00:47:20 +0200

    Added parse method for doubles

--- KNearsestNeightborsServer/Utils/ParseMethods.cpp
+++ KNearsestNeightborsServer/Utils/ParseMethods.cpp
@@ -18,3 +18,31 @@ bool parse(const string& integerString, int& parsedInteger) {
 
     return !stream.fail();
 }
+
+/*
+* Input:
+*	doublesString = The string to parse
+*	parsedDoubles = The doubles to parse the string into
+* Output:
+*	Whether the parse succeeded
+*/
+bool parse(const std::string& doublesString, vector<double>& parsedDoubles) {
+    stringstream stream(doublesString);
+
+    while (!stream.eof() && !stream.fail()) {
+        string currentString;
+        getline(stream, currentString, ',');
+
+        stringstream doubleStream(currentString);
+        double currentNumber;
+        doubleStream >> currentNumber;
+        
+        if (doubleStream.fail()) {
+            return false;
+        }
+        
+        parsedDoubles.push_back(currentNumber);
+    }
+
+    return true;
+}
--- KNearsestNeightborsServer/Utils/ParseMethods.h
+++ KNearsestNeightborsServer/Utils/ParseMethods.h
@@ -2,9 +2,11 @@
 #define __PARSE_METHODS_H
 
 #include <string>
+#include <vector>
 
 namespace app {
     bool parse(const std::string& stringToParse, int& parsedInt);
+    bool parse(const std::string& stringToParse, vector<double>& parsedDoubles);
 }
 
 #endif
